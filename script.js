c995db283d792dcd9c40964542db1d2247a7ffb8
Thiago Sanches Hohlenwerger <thiagosanchess52@gmail.com>
2025-01-03 13:46:47 -0300

    feature: Created only one email validation in the footer

---
+++ script.js
@@ -0,0 +1,21 @@
+function handleSubscribe(event) {
+    event.preventDefault();
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("response-message");
+
+    const validationEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!validationEmail.test(email)) {
+      message.textContent = "Por favor, insira um endereço de e-mail válido.";
+      message.style.color = "red";
+      return;
+    }
+
+    message.textContent = "E-mail enviado com sucesso!";
+    message.style.color = "green";
+
+    console.log("Email enviado:", email);
+
+
+    document.getElementById("email").value = "";
+  }
