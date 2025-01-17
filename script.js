d6d5c5b85f1641f447403614960fecb187b8eabb
Thiago Sanches Hohlenwerger <thiagosanchess52@gmail.com>
2025-01-17 16:48:06 -0300

    style: changing the size of card icons

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 function handleSubscribe(event) {
-    console.log("handleSubscribe chamada");
-    event.preventDefault();
+  console.log("handleSubscribe chamada");
+  event.preventDefault();
 
-    const email = document.getElementById("email").value;
-    const message = document.getElementById("response-message");
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("response-message");
 
-    const validationEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const validationEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    if (!validationEmail.test(email)) {
-      message.textContent = "Por favor, insira um endereço de e-mail válido.";
-      message.style.color = "red";
-      return;
-    }
-
-    message.textContent = "E-mail enviado com sucesso!";
-    message.style.color = "white";
-
-    console.log("Email enviado:", email);
-
-
-    document.getElementById("email").value = "";
+  if (!validationEmail.test(email)) {
+    message.textContent = "Por favor, insira um endereço de e-mail válido.";
+    message.style.color = "red";
+    return;
   }
+
+  message.textContent = "E-mail enviado com sucesso!";
+  message.style.color = "white";
+
+  console.log("Email enviado:", email);
+
+  document.getElementById("email").value = "";
+}
